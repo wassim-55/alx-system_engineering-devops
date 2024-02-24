73036d18e855209631cfaf9120247dc83153dcb8
wassim-55 <wchahed55@gmail.com>
2024-02-23 17:32:36 -0800

    kill_process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
-# create a process names killmenow
+# create a process named killmenow 
 
 exec { 'pkill killmenow' :
-  path => '/bin/',
-  command => 'pkill killmenow',
-}
+    path    => '/bin/',
+    command => 'pkill killmenow',
+    }
